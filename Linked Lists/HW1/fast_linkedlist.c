b0d66e27b42c11801f7075f22454c373ac7392b1
Toygarr <toygartanyel@hotmail.com>
2022-03-26 18:54:59 +0300

    new init and creating new dimensions func but still have memory access problems

--- Linked Lists/HW1/fast_linkedlist.c
+++ Linked Lists/HW1/fast_linkedlist.c
@@ -8,14 +8,21 @@ struct n{
 };
 typedef struct n node;
 
+// ***********************************************************************************************************
+// important to not get confused -> "next_row" is not for up-down, it basically points next element of same row
+// row can be considered as same linked list (same stage)
+// column can be considered as helper pointer to be able to reach next linked list (next stage)
+// "next_col" points connected element of next stage
+// ***********************************************************************************************************
+
 
 int count_dimension(int);
 void print(node *);
 //void search(node*, int);
-node* initialize_first_stage(node*, int, int); // creating ftwintirst linked list
+node* initialize(node*, int); // creating ftwintirst linked list
 node* insert(node*, int);
 node* delete(node*, int);
-node* createOtherDimensions(node*, int);
+node* createNewDimensions(node*, int);
 
 int main(){
 
@@ -25,84 +32,79 @@ int main(){
 
     dim = count_dimension(n) + 2; // this is the amount of stages for first initialization.
 
-    int* array = (int*)malloc(dim* sizeof(int));
-    node* head[dim]; //same for doing null every i in head[i];
-    for(i=0; i<dim-1; i++){
-        head[i] = (node*)malloc(sizeof(node));
-        head[i] -> next_row = NULL;
-        if( i+1 == dim-1)
-            head[i+1] -> next_row = NULL;
-        head[i] -> next_col = head[i+1];
-        // we will assume it's a special array of nodes to point stages
-    }
-    
+    node*head = (node*) malloc(sizeof(node));
+    head -> data = -1;
+    head -> next_row = NULL;
+    head -> next_col = NULL;
 
     for (i=0; i<n; i++){
         printf("enter new element: ");
         scanf("%d", &new_element);
-        array[i] = new_element;
-        head[0] = initialize_first_stage(head[0], new_element, dim);
+        head = initialize(head, new_element);
     }
+    print(head);
 
-    
-
-
+    head = createNewDimensions(head, dim);
 
 }
 
 int count_dimension(int n){
-
     if((n+1)/2 == 1)
         return 1;
-
     return count_dimension((n+1)/2) + 1;
 }
 
-int pick_new_elements(int n, int arr){
 
-}
-
-/*
 void print(node*head){
     int i = 0;
+    head = head->next_row;
     while(head != NULL){ // we will travel until we reach the NULL at the end
         i++;
         printf("%d. element: %d\n", i, head -> data);
-        head = head -> next;
+        head = head -> next_row;
     }
-}*/
-
-node * initialize_first_stage(node*head, int new_element, int dim){
-
-    node* current = head;
-    while(current -> next_row != NULL && new_element >= current -> next_row -> data)
-        current = current -> next_row;
-
-    // we create array of nodes for next stages to reach later on 
-    // we bound all temp nodes as columns
-    int i;
-    node* temp[dim];
-    for(i=0; i<dim-1; i++){
-        temp[i] = (node*)malloc(sizeof(node));
-        if(i+1 == dim-1)
-            temp[i+1] = (node*)malloc(sizeof(node));
-        temp[i] -> next_col = temp[i+1];
-    }
-    
-    // we initialize first dimension which is 0 for the program
-    temp[0] -> next_row = current -> next_row;
-    temp[0] -> data = new_element;
-    current -> next_row = temp[0];
-    return head;
-    
 }
 
-node * initialize_new_stages(int i, node* head[i], int new_element, int dim){
+node * initialize(node*head, int new_element){
+
     node* current = head;
     while(current -> next_row != NULL && new_element >= current -> next_row -> data)
         current = current -> next_row;
     
-    
-    
-    return initialize_new_stages(i++, head[i], new_element, dim);
+    // we need temporary box to not lose linked list at the space while changing pointers
+    // we have to point the linked list if we break it without adding new point, we cannot reach that list again
+    node * temp = (node*) malloc(sizeof(node));
+
+    temp -> next_row = current -> next_row;
+    temp -> next_col = NULL;
+    temp -> data = new_element;
+    current -> next_row = temp;
+    return head;
+}
+
+
+node * createNewDimensions(node* head, int dim){
+    printf("Error");
+    node * new_stage_head = (node*)malloc(sizeof(node));
+    new_stage_head -> data = -1;
+    new_stage_head -> next_row = NULL;
+    new_stage_head -> next_col = head;
+    printf("Error");
+    int i;
+    node*iter = head->next_row;
+    for (i=0; i<dim; i=i+2){
+        int new_element;
+        while(iter -> next_row != NULL){
+            new_element = iter->data;
+            new_stage_head = initialize(new_stage_head, new_element);
+            node*finder=new_stage_head;
+            while(finder->next_row->data != new_element || finder->next_row != NULL)
+                finder = finder -> next_row;
+
+            finder->next_row->next_col = iter;
+            iter = iter -> next_row -> next_row;
+        }
+    }
+
+    return createNewDimensions(new_stage_head, ((dim+1)/2));
 }

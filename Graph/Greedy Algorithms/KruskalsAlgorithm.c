50d07554482515621b41388ff8c431846455986e
Toygarr <toygartanyel@hotmail.com>
2022-05-06 17:06:01 +0300

    kruskals' homework edition

--- Graph/Greedy Algorithms/KruskalsAlgorithm.c
+++ Graph/Greedy Algorithms/KruskalsAlgorithm.c
@@ -22,6 +22,25 @@ int find(int belongs[], int vertexno);
 void applyUnion(int belongs[], int c1, int c2);
 void sort();
 void print();
+void update_edges();
+void add_new_edge();
+
+
+int main() {
+  int i, j, total_cost;
+
+  FILE *fp = fopen("test.txt", "r");
+  fscanf(fp, "%d", &elist.n);
+  
+  elist.n=0;
+  while(!feof(fp)){
+    fscanf(fp, "%d %d %d", &elist.data[elist.n].u, &elist.data[elist.n].v, &elist.data[elist.n].w);
+    elist.n++;
+  }
+  kruskalAlg();
+  print();
+  add_new_edge();
+}
 
 // Applying Kruskal Algorithm
 void kruskalAlg() {
@@ -37,13 +56,27 @@ void kruskalAlg() {
     cno1 = find(belongs, elist.data[i].u);
     cno2 = find(belongs, elist.data[i].v);
 
+    printf("\ndöngü %d", i);
+    printf("\ncno1: %d - cno2: %d\n", cno1, cno2);
+
     if (cno1 != cno2) {
       spanlist.data[spanlist.n] = elist.data[i];
       spanlist.n++;
       applyUnion(belongs, cno1, cno2);
     }
+
+      
+  printf("\nBELONG %d\n", i);
+  for(j=0; j<elist.n; j++){
+    printf("bel%d: %d\n",j, belongs[j]);
   }
 
+  }
+
+  printf("LAST BELONGS:\n");
+  for(i=0; i<elist.n; i++){
+    printf("bel%d: %d\n",i, belongs[i]);
+  }
 }
 
 
@@ -77,6 +110,47 @@ void sort() {
 
 }
 
+void update_edges(int x){
+  spanlist.data[x].u = elist.data[elist.n].u;
+  spanlist.data[x].v = elist.data[elist.n].v;
+  spanlist.data[x].w = elist.data[elist.n].w;
+}
+
+void add_new_edge(){
+  printf("\n\tADDING NEW EDGE\n\tEnter the source, destination point and weight. Leave a space in between: ");
+  scanf("%d %d %d", &elist.data[elist.n].u, &elist.data[elist.n].v, &elist.data[elist.n].w);
+  sort();
+  int i, j;
+
+  for(i=0; i<spanlist.n; i++){
+    if(elist.data[elist.n].u == spanlist.data[i].v){
+      for(j=0; j<spanlist.n; j++){
+        if(spanlist.data[i].u == spanlist.data[j].u && elist.data[elist.n].v == spanlist.data[j].v && elist.data[elist.n].w < spanlist.data[j].w){
+          update_edges(j);
+        }
+      }
+    }
+    else if(elist.data[elist.n].v == spanlist.data[i].u){  
+      for(j=0; j<spanlist.n; j++){
+        if(spanlist.data[i].v == spanlist.data[j].v && elist.data[elist.n].u == spanlist.data[j].u && elist.data[elist.n].w < spanlist.data[j].w){
+          update_edges(j);
+      }
+    }
+  }
+    else if(elist.data[elist.n].u == spanlist.data[i].u){
+      for(j=0; j<spanlist.n; j++){
+        if(spanlist.data[i].v == spanlist.data[j].u && elist.data[elist.n].v == spanlist.data[j].v &&elist.data[elist.n].w < spanlist.data[j].w){
+          update_edges(j);
+        }
+      }
+    }
+  }
+  printf("\nNEW MST AFTER ADDING EDGE");
+  print();
+}
+
+
+
 // Printing the result
 void print() {
   int i, cost = 0;
@@ -87,20 +161,4 @@ void print() {
   }
 
   printf("\nSpanning tree cost: %d", cost);
-}
-
-int main() {
-  int i, j, total_cost;
-
-  FILE *fp = fopen("test.txt", "r");
-  fscanf(fp, "%d", &elist.n);
-  
-  elist.n=0;
-  while(!feof(fp)){
-    fscanf(fp, "%d %d %d", &elist.data[elist.n].u, &elist.data[elist.n].v, &elist.data[elist.n].w);
-    elist.n++;
-  }
-
-  kruskalAlg();
-  print();
 }
